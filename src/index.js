5e98736530f556c366b16abb26ae836f43692c85
edge <sort@programmer.net>
2015-08-27 10:19:06 +0900

    move select to react directory

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import For from './reducer/for';
 
 import Context from './react/context';
 import connect from './react/connect';
-import select from './factory/select';
+import select from './react/select';
 
 export {
 	Store, Reducer, Filter, For,
